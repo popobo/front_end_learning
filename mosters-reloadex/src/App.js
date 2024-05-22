83aa89b40e61912d0513ab3208457649331b6780
popobo <331361996@qq.com>
2024-05-22 22:58:37 +0800

    20240522

--- mosters-reloadex/src/App.js
+++ mosters-reloadex/src/App.js
@@ -4,19 +4,25 @@ import "./App.css";
 
 class App extends Component {
   constructor() {
+    console.log("constructor");
     super();
     this.state = {
       monsters: [],
+      filterdMonsters: [],
     };
   }
   // Called after the component is mounted to the DOM
   componentDidMount() {
+    console.log("componentDidMount");
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) => {
         this.setState(
           () => {
-            return { monsters: users };
+            return {
+              monsters: users,
+              filterdMonsters: users,
+            };
           },
           () => {
             console.log(this.state);
@@ -26,9 +32,29 @@ class App extends Component {
   }
 
   render() {
+    console.log("render");
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={(event) => {
+            this.setState(
+              () => {
+                return {
+                  filterdMonsters: this.state.monsters.filter((monster) => {
+                    return monster.name
+                      .toLowerCase()
+                      .includes(event.target.value.toLowerCase());
+                  }),
+                };
+              },
+              () => {}
+            );
+          }}
+        />
+        {this.state.filterdMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
