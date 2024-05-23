7774c8d218936a0e2b9c4f231cc4de386009f08a
popobo <331361996@qq.com>
2024-05-23 23:02:01 +0800

    20240523

--- mosters-reloadex/src/components/card-list/card-list.component.jsx
+++ mosters-reloadex/src/components/card-list/card-list.component.jsx
@@ -1,17 +1,25 @@
 import { Component } from "react";
+import "./card-list.styles.css";
 
 class CardList extends Component {
   render() {
     // render twice
     console.log("render from CardList");
     const { monsters } = this.props;
+
     return (
-      <div>
+      <div className="card-list">
         {monsters.map((monster) => {
+          const { id, name, email } = monster;
+
           return (
-            <div key={monster.id}>
-              <h1>{monster.name}</h1>
-              <p>{monster.email}</p>
+            <div className="card-container" key={id}>
+              <img
+                alt={`monster ${name}`}
+                src={`https://robohash.org/${id}?set=set2&size=180x180`}
+              />
+              <h2>{name}</h2>
+              <p>{email}</p>
             </div>
           );
         })}
