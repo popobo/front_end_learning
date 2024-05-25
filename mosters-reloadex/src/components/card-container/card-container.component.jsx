970f2be0e462e78abbf85b7efb0da9e8e959e1be
popobo <331361996@qq.com>
2024-05-25 11:05:09 +0800

    20240525

--- mosters-reloadex/src/components/card-container/card-container.component.jsx
+++ mosters-reloadex/src/components/card-container/card-container.component.jsx
@@ -1,21 +1,18 @@
 import { Component } from "react";
 import "./card-container.styles.css";
 
-class CardContainer extends Component {
-  render() {
-    const { id, name, email } = this.props.monster;
-
-    return (
-      <div className="card-container">
-        <img
-          alt={`monster ${name}`}
-          src={`https://robohash.org/${id}?set=set2&size=180x180`}
-        />
-        <h2>{name}</h2>
-        <p>{email}</p>
-      </div>
-    );
-  }
-}
+const CardContainer = ({ monster }) => {
+  const { id, name, email } = monster;
+  return (
+    <div className="card-container">
+      <img
+        alt={`monster ${name}`}
+        src={`https://robohash.org/${id}?set=set2&size=180x180`}
+      />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
+};
 
 export default CardContainer;
--- mosters-reloadex/src/components/card-list/card-list.component.jsx
+++ mosters-reloadex/src/components/card-list/card-list.component.jsx
@@ -1,21 +1,14 @@
-import { Component } from "react";
 import "./card-list.styles.css";
 import CardContainer from "../card-container/card-container.component";
 
-class CardList extends Component {
-  render() {
-    // render twice
-    console.log("render from CardList");
-    const { monsters } = this.props;
-
-    return (
-      <div className="card-list">
-        {monsters.map((monster) => {
-          return <CardContainer key={monster.id} monster={monster} />;
-        })}
-      </div>
-    );
-  }
-}
+const CardList = ({ monsters }) => {
+  return (
+    <div className="card-list">
+      {monsters.map((monster) => (
+        <CardContainer key={monster.id} monster={monster} />
+      ))}
+    </div>
+  );
+};
 
 export default CardList;
