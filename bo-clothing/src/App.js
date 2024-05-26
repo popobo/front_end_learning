977f9057fc0aba4c3861a2d05edcec3e77d9b9d9
popobo <331361996@qq.com>
2024-05-26 16:31:44 +0800

    feat: add navigation bar

--- bo-clothing/src/App.js
+++ bo-clothing/src/App.js
@@ -1,5 +1,6 @@
-import Home from "./components/routes/home/home.component";
+import Home from "./routes/home/home.component";
 import { Routes, Route } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component";
 
 const Shop = () => {
   return <h1>Shop Page</h1>;
@@ -8,8 +9,9 @@ const Shop = () => {
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />}>
-        <Route path="/shop" element={<Shop />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />}></Route>
+        <Route path="shop" element={<Shop />} />
       </Route>
     </Routes>
   );
