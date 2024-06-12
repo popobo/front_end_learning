60bb096021a5b0f34543044a1a2ef1349dae2768
popobo <331361996@qq.com>
2024-06-12 23:03:40 +0800

    feat: Add NotFound component for product reviews
    This commit adds a NotFound component for handling cases where a review or product is not found. The component is added to both the top-level `not-found.tsx` file and the nested `not-found.tsx` file within the product reviews directory. Additionally, the `page.tsx` file is updated to import the `notFound` function from the `next/navigation` module and use it to handle cases where the review ID is greater than 100.

--- next.js_course/next_demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ next.js_course/next_demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,3 +1,5 @@
+import { notFound } from "next/navigation";
+
 export default function ReviewDetails({
   params,
 }: {
@@ -6,6 +8,9 @@ export default function ReviewDetails({
     reviewId: string;
   };
 }) {
+  if (parseInt(params.reviewId) > 100) {
+    notFound();
+  }
   return (
     <h1>
       Review Details {params.reviewId} for product {params.productId}
