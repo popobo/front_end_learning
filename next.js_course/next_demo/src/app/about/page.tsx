5937ddf23746bd8ad23f40f2af872fb789bd0a9e
popobo <331361996@qq.com>
2024-06-17 21:54:21 +0800

    feat: metadata and dynamic metadata

--- next.js_course/next_demo/src/app/about/page.tsx
+++ next.js_course/next_demo/src/app/about/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: "About us",
+};
+
 export default function About() {
   return <h1>About us</h1>;
 }
--- next.js_course/next_demo/src/app/products/[productId]/page.tsx
+++ next.js_course/next_demo/src/app/products/[productId]/page.tsx
@@ -1,9 +1,21 @@
-export default function ProductDetail({
-  params,
-}: {
+import { Metadata } from "next";
+type Props = {
   params: {
     productId: string;
   };
-}) {
+};
+
+export default function ProductDetail({ params }: Props) {
   return <h1>Product Detail {params.productId}</h1>;
 }
+
+export const generateMetadata = async ({ params }: Props): Metadata => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`iPhone ${params.productId}`);
+    }, 1000);
+  });
+  return {
+    title: `Product ${title}`,
+  };
+};
