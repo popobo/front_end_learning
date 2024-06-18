2870ece9d91751551c36c347822b115a09cda7a1
popobo <331361996@qq.com>
2024-06-19 00:22:12 +0800

    feat: Link Component Navigation

--- next.js_course/next_demo/src/app/products/page.tsx
+++ next.js_course/next_demo/src/app/products/page.tsx
@@ -1,10 +1,23 @@
+import Link from "next/link";
+
 export default function ProductList() {
+  const productId = 100;
   return (
     <div>
+      <Link href="/">Home</Link>
       <h1>Product List</h1>
-      <h1>Product 1</h1>
-      <h1>Product 2</h1>
-      <h1>Product 3</h1>
+      <h1>
+        <Link href="/products/1">Product 1</Link>
+      </h1>
+      <h1>
+        <Link href="/products/2">Product 2</Link>
+      </h1>
+      <h1>
+        <Link href="/products/3">Product 3</Link>
+      </h1>
+      <h1>
+        <Link href={`products/${productId}`}>Product {productId}</Link>
+      </h1>
     </div>
   );
 }
