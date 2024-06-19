d21d1202b69aa48ba93a9d22b55f35d9bea83d6d
popobo <331361996@qq.com>
2024-06-20 00:29:02 +0800

    feat: loading.tsx and error.tsx

---
+++ next.js_course/next_demo/src/app/blog/loading.tsx
@@ -0,0 +1,3 @@
+export default function Loading() {
+  return <h1>Loading...Loading...Loading...Loading...Loading...</h1>;
+}
--- next.js_course/next_demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ next.js_course/next_demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,5 +1,9 @@
 import { notFound } from "next/navigation";
 
+function getRandomInt(count: number) {
+  return Math.floor(Math.random() * count);
+}
+
 export default function ReviewDetails({
   params,
 }: {
@@ -8,6 +12,12 @@ export default function ReviewDetails({
     reviewId: string;
   };
 }) {
+  const random = getRandomInt(2);
+
+  if (random === 1) {
+    throw new Error("Error loading review");
+  }
+
   if (parseInt(params.reviewId) > 100) {
     notFound();
   }
