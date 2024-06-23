82a6848c10016c9a52065a9698d324dcd8875f57
popobo <331361996@qq.com>
2024-06-23 17:19:18 +0800

    feat: recovering from errors

--- next.js_course/next_demo/src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ next.js_course/next_demo/src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,4 +1,15 @@
 "use client";
-export default function ErrorBoundary({ error }: { error: Error }) {
-  return <div>Error in reviewId. {error.message}</div>;
+export default function ErrorBoundary({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  return (
+    <div>
+      Error in reviewId. {error.message}
+      <button onClick={reset}>Try again</button>
+    </div>
+  );
 }
