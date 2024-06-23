64e9045e4e2eb5705bc1799c24deb6e87d3b1a03
popobo <331361996@qq.com>
2024-06-23 16:24:37 +0800

    fix: promise issue

--- next.js_course/next_demo/src/app/products/[productId]/page.tsx
+++ next.js_course/next_demo/src/app/products/[productId]/page.tsx
@@ -9,7 +9,9 @@ export default function ProductDetail({ params }: Props) {
   return <h1>Product Detail {params.productId}</h1>;
 }
 
-export const generateMetadata = async ({ params }: Props): Metadata => {
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
   const title = await new Promise((resolve) => {
     setTimeout(() => {
       resolve(`iPhone ${params.productId}`);
