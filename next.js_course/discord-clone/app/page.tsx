dcc66d32addba8552228f563d9492497604c9076
popobo <331361996@qq.com>
2024-06-25 01:12:32 +0800

    feat: test cn

--- next.js_course/discord-clone/app/page.tsx
+++ next.js_course/discord-clone/app/page.tsx
@@ -1,10 +1,18 @@
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+const state = true;
 
 export default function Home() {
   return (
     <div>
       <p className="text-3xl font-bold text-indigo-500">Hello Discord Clone!</p>
       <Button variant="ghost" className="bg-indigo-500">
+        Click me
+      </Button>
+      <Button
+        className={cn("bg-indigo-500", state ? "text-white" : "text-black")}
+      >
         Click me
       </Button>
     </div>
