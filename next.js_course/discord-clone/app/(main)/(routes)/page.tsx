0f90e8ba68800935c3a686b3f5ba77cbaeae4e4e
popobo <331361996@qq.com>
2024-06-27 22:30:22 +0800

    feat: add auth component and  and dark & light mode setup

--- next.js_course/discord-clone/app/(main)/(routes)/page.tsx
+++ next.js_course/discord-clone/app/(main)/(routes)/page.tsx
@@ -1,12 +1,15 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import { UserButton } from "@clerk/nextjs";
 
 const state = true;
 
 export default function Home() {
   return (
     <div>
-      This is a protected route!
+      <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   );
 }
